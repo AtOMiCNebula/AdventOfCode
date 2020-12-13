77b708b73c0bee8ce4b6f4a4d2a7f60f20d506a2
Jeff Weiner <nebula@planet-nebula.com>
2020-12-13 13:39:48 -0800

    Day 13 Part 1

---
+++ src/AdventOfCode.Tests/Year2020/Day13Tests.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="Day13Tests.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2020
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NebulousIndustries.AdventOfCode.Year2020;
+
+    [TestClass]
+    public class Day13Tests : DayTests<Day13>
+    {
+        [TestMethod]
+        public void TestAnswers()
+        {
+            Assert.AreEqual(3464, this.Day.Part1());
+            Assert.AreEqual(-1, this.Day.Part2());
+        }
+    }
+}
