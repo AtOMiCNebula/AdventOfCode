d284a94fad7ea9062e016457e666a5d8b3c759e6
Jeff Weiner <nebula@planet-nebula.com>
2020-12-21 14:45:34 -0800

    Day 15 Part 1 complete
    ...probably need to rewrite for Part 2 though. O:-)

---
+++ src/AdventOfCode.Tests/Year2020/Day15Tests.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="Day15Tests.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2020
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NebulousIndustries.AdventOfCode.Year2020;
+
+    [TestClass]
+    public class Day15Tests : DayTests<Day15>
+    {
+        [TestMethod]
+        public void TestAnswers()
+        {
+            Assert.AreEqual(1294, this.Day.Part1());
+            Assert.AreEqual(-1, this.Day.Part2());
+        }
+    }
+}
---
+++ src/AdventOfCode/Year2020/Day15.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="Day15.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Year2020
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Day15 : DayBase
+    {
+        public override int Number => 15;
+
+        public override long Part1()
+        {
+            List<int> numbers = this.GetInputRaw().First().Split(',').Select(int.Parse).ToList();
+            return GetSeries(numbers).Take(2020).Last();
+        }
+
+        public override long Part2()
+        {
+            return -1;
+        }
+
+        public static IEnumerable<int> GetSeries(IList<int> startingNumbers)
+        {
+            List<int> numbers = new List<int>(startingNumbers);
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                yield return numbers[i];
+            }
+
+            for (int i = numbers.Count; true; i++)
+            {
+                int next;
+                int previous = numbers[i - 1];
+                if (numbers.Count(num => num == previous) == 1)
+                {
+                    next = 0;
+                }
+                else
+                {
+                    int lastIndexOf = numbers.SkipLast(1).ToList().LastIndexOf(previous);
+                    next = i - 1 - lastIndexOf;
+                }
+
+                numbers.Add(next);
+                yield return next;
+            }
+        }
+    }
+}
