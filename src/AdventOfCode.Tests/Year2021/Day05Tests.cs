5e49879cfc19c5bad0239f9f691dd7f7505f43d2
Jeff Weiner <nebula@planet-nebula.com>
2021-12-19 00:29:13 -0500

    2021 Day 5 complete

---
+++ src/AdventOfCode.Tests/Year2021/Day05Tests.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Day05Tests.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2021
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NebulousIndustries.AdventOfCode.Year2021;
+
+    [TestClass]
+    public class Day05Tests : DayTests<Day05>
+    {
+        public override long Part1Answer => 5632;
+
+        public override long Part2Answer => 22213;
+    }
+}
---
+++ src/AdventOfCode/Year2021/Day05.cs
@@ -0,0 +1,79 @@
+﻿// <copyright file="Day05.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Year2021
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+
+    public class Day05 : DayBase<LineSegment>
+    {
+        public override long Part1()
+        {
+            return this.CountOverlaps(2, false);
+        }
+
+        public override long Part2()
+        {
+            return this.CountOverlaps(2, true);
+        }
+
+        [SuppressMessage("Performance", "CA1814:Prefer jagged arrays over multidimensional", Justification = "Sheeeeeesh.")]
+        protected int CountOverlaps(int threshold, bool allowDiagonals)
+        {
+            IEnumerable<LineSegment> segments = this.GetInput();
+            int maxY = segments.Max(s => Math.Max(s.Y1, s.Y2)) + 1;
+            int maxX = segments.Max(s => Math.Max(s.X1, s.X2)) + 1;
+
+            int overlaps = 0;
+            int[,] grid = new int[maxY, maxX];
+            foreach (LineSegment segment in segments)
+            {
+                if (!allowDiagonals && segment.X1 != segment.X2 && segment.Y1 != segment.Y2)
+                {
+                    continue;
+                }
+
+                int dist = Math.Max(Math.Abs(segment.Y2 - segment.Y1), Math.Abs(segment.X2 - segment.X1));
+                int yIncr = segment.Y1 == segment.Y2 ? 0 : segment.Y1 < segment.Y2 ? 1 : -1;
+                int xIncr = segment.X1 == segment.X2 ? 0 : segment.X1 < segment.X2 ? 1 : -1;
+                for (int d = 0; d <= dist; d++)
+                {
+                    int yCurrent = segment.Y1 + (yIncr * d);
+                    int xCurrent = segment.X1 + (xIncr * d);
+                    grid[yCurrent, xCurrent]++;
+                    if (grid[yCurrent, xCurrent] == threshold)
+                    {
+                        overlaps++;
+                    }
+                }
+            }
+
+            return overlaps;
+        }
+    }
+
+    public class LineSegment : IDayInput
+    {
+        public int X1 { get; set; }
+
+        public int Y1 { get; set; }
+
+        public int X2 { get; set; }
+
+        public int Y2 { get; set; }
+
+        public bool Load(string input)
+        {
+            string[] split = input.Split(new string[] { ",", " -> " }, StringSplitOptions.None);
+            this.X1 = int.Parse(split[0]);
+            this.Y1 = int.Parse(split[1]);
+            this.X2 = int.Parse(split[2]);
+            this.Y2 = int.Parse(split[3]);
+            return false;
+        }
+    }
+}
