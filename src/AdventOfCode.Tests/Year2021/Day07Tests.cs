7914915a05237f831b38361cc8ef2557da65c026
Jeff Weiner <nebula@planet-nebula.com>
2021-12-19 14:22:32 -0500

    2021 Day 7 complete

---
+++ src/AdventOfCode.Tests/Year2021/Day07Tests.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Day07Tests.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2021
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NebulousIndustries.AdventOfCode.Year2021;
+
+    [TestClass]
+    public class Day07Tests : DayTests<Day07>
+    {
+        public override long Part1Answer => 344535;
+
+        public override long Part2Answer => 95581659;
+    }
+}
---
+++ src/AdventOfCode/Year2021/Day07.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="Day07.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Year2021
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Day07 : DayBase
+    {
+        public override long Part1()
+        {
+            return this.CalculateMinimumFuel(true);
+        }
+
+        public override long Part2()
+        {
+            return this.CalculateMinimumFuel(false);
+        }
+
+        protected int CalculateMinimumFuel(bool linearFuel)
+        {
+            IEnumerable<int> crabs = this.GetInputRaw().Single().Split(",").Select(int.Parse);
+            int maxPosition = crabs.Max();
+            int minFuel = int.MaxValue;
+            for (int i = 0; i <= maxPosition; i++)
+            {
+                int fuel = crabs.Sum(c => FuelForDistance(c, i, linearFuel));
+
+                if (fuel < minFuel)
+                {
+                    minFuel = fuel;
+                }
+            }
+
+            return minFuel;
+        }
+
+        protected static int FuelForDistance(int c, int n, bool linearFuel)
+        {
+            int f = Math.Abs(c - n);
+            if (!linearFuel)
+            {
+                f = ((f * f) + f) / 2;
+            }
+
+            return f;
+        }
+    }
+}
