4f0bca6502a63ff1b08272829ec1dee8b70d9cbf
Jeff Weiner <nebula@planet-nebula.com>
2021-12-19 13:39:43 -0500

    2021 Day 6 complete

---
+++ src/AdventOfCode/Year2021/Day06.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="Day06.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Year2021
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Day06 : DayBase
+    {
+        public override long Part1()
+        {
+            return this.CountFishAfterDays(80);
+        }
+
+        public override long Part2()
+        {
+            return this.CountFishAfterDays(256);
+        }
+
+        protected long CountFishAfterDays(int days)
+        {
+            List<long> fishes = new() { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            foreach (int fish in this.GetInputRaw().Single().Split(",").Select(int.Parse))
+            {
+                fishes[fish]++;
+            }
+
+            for (int d = 0; d < days; d++)
+            {
+                fishes = new() { fishes[1], fishes[2], fishes[3], fishes[4], fishes[5], fishes[6], fishes[7] + fishes[0], fishes[8], fishes[0] };
+            }
+
+            return fishes.Sum();
+        }
+    }
+}
