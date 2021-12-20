8306a91074e221f64730a11e7bd3497d3440547e
Jeff Weiner <nebula@planet-nebula.com>
2021-12-20 17:39:09 -0500

    2021 Day 11 complete

---
+++ src/AdventOfCode.Tests/Year2021/Day11Tests.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Day11Tests.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2021
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NebulousIndustries.AdventOfCode.Year2021;
+
+    [TestClass]
+    public class Day11Tests : DayTests<Day11>
+    {
+        public override long Part1Answer => 1757;
+
+        public override long Part2Answer => 422;
+    }
+}
---
+++ src/AdventOfCode/Year2021/Day11.cs
@@ -0,0 +1,101 @@
+﻿// <copyright file="Day11.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Year2021
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Day11 : DayBase
+    {
+        public override long Part1()
+        {
+            int[][] octopi = this.GetInputRaw().Select(s => s.ToCharArray().Select(c => (int)c - '0').ToArray()).ToArray();
+
+            int flashes = 0;
+            for (int s = 0; s < 100; s++)
+            {
+                flashes += Step(octopi);
+            }
+
+            return flashes;
+        }
+
+        public override long Part2()
+        {
+            int[][] octopi = this.GetInputRaw().Select(s => s.ToCharArray().Select(c => (int)c - '0').ToArray()).ToArray();
+
+            for (int s = 0; ; s++)
+            {
+                int flashes = Step(octopi);
+                if (flashes == octopi.Length * octopi[0].Length)
+                {
+                    return s + 1;
+                }
+            }
+        }
+
+        protected static int Step(int[][] octopi)
+        {
+            int flashes = 0;
+
+            // Increase energy levels
+            Queue<(int Y, int X)> flashers = new();
+            for (int y = 0; y < octopi.Length; y++)
+            {
+                for (int x = 0; x < octopi[y].Length; x++)
+                {
+                    IncreaseEnergy(octopi, y, x, flashers);
+                }
+            }
+
+            // Handle flashes (and potential cascading)
+            Queue<(int Y, int X)> highEnergyOctopi = new();
+            while (flashers.Any())
+            {
+                (int yFlash, int xFlash) = flashers.Dequeue();
+                highEnergyOctopi.Enqueue((yFlash, xFlash));
+                flashes++;
+
+                for (int yDelta = -1; yDelta <= 1; yDelta++)
+                {
+                    int y = yFlash + yDelta;
+                    if (0 > y || y >= octopi.Length)
+                    {
+                        continue;
+                    }
+
+                    for (int xDelta = -1; xDelta <= 1; xDelta++)
+                    {
+                        int x = xFlash + xDelta;
+                        if (0 > x || x >= octopi[y].Length)
+                        {
+                            continue;
+                        }
+
+                        IncreaseEnergy(octopi, y, x, flashers);
+                    }
+                }
+            }
+
+            // Decrease high-energy octopi
+            while (highEnergyOctopi.Any())
+            {
+                (int y, int x) = highEnergyOctopi.Dequeue();
+                octopi[y][x] = 0;
+            }
+
+            return flashes;
+        }
+
+        protected static void IncreaseEnergy(int[][] octopi, int y, int x, Queue<(int Y, int X)> flashers)
+        {
+            octopi[y][x]++;
+            if (octopi[y][x] == 10)
+            {
+                flashers.Enqueue((y, x));
+            }
+        }
+    }
+}
