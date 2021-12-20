a336764abdfeb1a83416f877442700417c675bd1
Jeff Weiner <nebula@planet-nebula.com>
2021-12-20 00:17:59 -0500

    2021 Day 10 complete

---
+++ src/AdventOfCode.Tests/Year2021/Day10Tests.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Day10Tests.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2021
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NebulousIndustries.AdventOfCode.Year2021;
+
+    [TestClass]
+    public class Day10Tests : DayTests<Day10>
+    {
+        public override long Part1Answer => 367227;
+
+        public override long Part2Answer => 3583341858;
+    }
+}
---
+++ src/AdventOfCode/Year2021/Day10.cs
@@ -0,0 +1,94 @@
+﻿// <copyright file="Day10.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Year2021
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Day10 : DayBase
+    {
+        public static IReadOnlyDictionary<char, char> Openers { get; } = new Dictionary<char, char>()
+        {
+            { '(', ')' },
+            { '[', ']' },
+            { '{', '}' },
+            { '<', '>' },
+        };
+
+        public static IReadOnlyDictionary<char, (int CorruptionScore, int AutocompleteScore)> Closers { get; } = new Dictionary<char, (int CorruptionScore, int AutocompleteScore)>()
+        {
+            { ')', (3, 1) },
+            { ']', (57, 2) },
+            { '}', (1197, 3) },
+            { '>', (25137, 4) },
+        };
+
+        public override long Part1()
+        {
+            return this.CalculateScore(true);
+        }
+
+        public override long Part2()
+        {
+            return this.CalculateScore(false);
+        }
+
+        public long CalculateScore(bool scoreCorruptedLines)
+        {
+            IEnumerable<string> lines = this.GetInputRaw();
+            List<long> scores = new();
+            foreach (string line in lines)
+            {
+                long score = 0;
+                Stack<char> groups = new();
+                bool corrupted = false;
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (Openers.ContainsKey(line[i]))
+                    {
+                        groups.Push(Openers[line[i]]);
+                    }
+                    else if (line[i] == groups.Peek())
+                    {
+                        groups.Pop();
+                    }
+                    else if (Closers.ContainsKey(line[i]))
+                    {
+                        if (scoreCorruptedLines)
+                        {
+                            score += Closers[line[i]].CorruptionScore;
+                        }
+
+                        corrupted = true;
+                        break;
+                    }
+                }
+
+                if (!corrupted && !scoreCorruptedLines)
+                {
+                    foreach (char closer in groups)
+                    {
+                        score = (score * 5) + Closers[closer].AutocompleteScore;
+                    }
+                }
+
+                if (score > 0)
+                {
+                    scores.Add(score);
+                }
+            }
+
+            if (scoreCorruptedLines)
+            {
+                return scores.Sum();
+            }
+            else
+            {
+                scores.Sort();
+                return scores[scores.Count / 2];
+            }
+        }
+    }
+}
