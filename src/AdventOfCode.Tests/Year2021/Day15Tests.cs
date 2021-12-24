7ddc96e0761ba0661e99a2cb31123d5daf8e28c4
Jeff Weiner <nebula@planet-nebula.com>
2021-12-24 18:37:37 -0500

    2021 Day 15 complete

---
+++ src/AdventOfCode.Tests/Year2021/Day15Tests.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Day15Tests.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2021
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NebulousIndustries.AdventOfCode.Year2021;
+
+    [TestClass]
+    public class Day15Tests : DayTests<Day15>
+    {
+        public override long Part1Answer => 702;
+
+        public override long Part2Answer => 2955;
+    }
+}
