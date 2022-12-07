3fb2eff0812693ca8f1ad25fba401f06d90eec18
Jeff Weiner <nebula@planet-nebula.com>
2022-12-06 21:03:29 -0800

    2022 Day 6 complete

---
+++ src/AdventOfCode.Tests/Year2022/Day06Tests.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Day06Tests.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2022
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NebulousIndustries.AdventOfCode.Year2022;
+
+    [TestClass]
+    public class Day06Tests : DayTests<Day06, long>
+    {
+        public override long Part1Answer => 1210;
+
+        public override long Part2Answer => 3476;
+    }
+}
---
+++ src/AdventOfCode/Year2022/Day06.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="Day06.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Year2022
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Day06 : DayBase
+    {
+        public override long Part1()
+        {
+            string input = this.GetInputRaw().Single();
+            return Day06.FindMarker(input, 4);
+        }
+
+        public override long Part2()
+        {
+            string input = this.GetInputRaw().Single();
+            return Day06.FindMarker(input, 14);
+        }
+
+        public static int FindMarker(string input, int uniques)
+        {
+            for (int i = 0; i < input.Length - uniques + 1; i++)
+            {
+                HashSet<char> chars = new();
+                chars.Add(input[i]);
+
+                bool shouldContinue = false;
+                for (int j = 1; j < uniques; j++)
+                {
+                    if (!chars.Add(input[i + j]))
+                    {
+                        shouldContinue = true;
+                        break;
+                    }
+                }
+
+                if (shouldContinue)
+                {
+                    continue;
+                }
+
+                return i + uniques;
+            }
+            return -1;
+        }
+    }
+}
