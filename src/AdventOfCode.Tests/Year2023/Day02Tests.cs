4a796c7832127569272d2ec439ef7eaf1ad2124f
Jeff Weiner <nebula@planet-nebula.com>
2023-12-22 18:22:12 -0500

    2023 Day 2 complete

---
+++ src/AdventOfCode.Tests/Year2023/Day02Tests.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Day02Tests.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2023;
+
+using NebulousIndustries.AdventOfCode.Year2023;
+
+public class Day02Tests : DayTests<Day02>
+{
+    public override long Part1Answer => 2268;
+
+    public override long Part2Answer => 63542;
+}
---
+++ src/AdventOfCode/Year2023/Day02.cs
@@ -0,0 +1,74 @@
+﻿// <copyright file="Day02.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Year2023;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Day02 : DayBase<CubeGameResult>
+{
+    public override long Part1()
+    {
+        return this
+            .GetInput()
+            .Where(g => g.Rolls.All(r =>
+                r.Red <= 12 &&
+                r.Green <= 13 &&
+                r.Blue <= 14))
+            .Sum(g => g.GameID);
+    }
+
+    public override long Part2()
+    {
+        return this
+            .GetInput()
+            .Select(g =>
+                g.Rolls.Max(r => r.Red) *
+                g.Rolls.Max(r => r.Green) *
+                g.Rolls.Max(r => r.Blue))
+            .Sum();
+    }
+}
+
+public class CubeGameResult : IDayInput
+{
+    public int GameID { get; set; }
+
+    public IList<(int Red, int Green, int Blue)> Rolls { get; } = new List<(int Red, int Green, int Blue)>();
+
+    public bool Load(string input)
+    {
+        this.GameID = int.Parse(input.Split(':')[0].Substring(5));
+
+        input = input.Substring(input.IndexOf(": ") + 2);
+        foreach (string round in input.Split(';', StringSplitOptions.TrimEntries))
+        {
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+            foreach (string roll in round.Split(',', StringSplitOptions.TrimEntries))
+            {
+                string[] rollSplit = roll.Split(' ');
+                switch (rollSplit[1])
+                {
+                    case "red":
+                        red = int.Parse(rollSplit[0]);
+                        break;
+                    case "green":
+                        green = int.Parse(rollSplit[0]);
+                        break;
+                    case "blue":
+                        blue = int.Parse(rollSplit[0]);
+                        break;
+                }
+            }
+
+            this.Rolls.Add((red, green, blue));
+        }
+
+        return false;
+    }
+}
