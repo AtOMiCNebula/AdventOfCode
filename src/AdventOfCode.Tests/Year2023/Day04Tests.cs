2d659c5bdd7581878169ae720ffa9218771723f5
Jeff Weiner <nebula@planet-nebula.com>
2023-12-24 12:59:59 -0500

    2023 Day 4 complete

---
+++ src/AdventOfCode.Tests/Year2023/Day04Tests.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Day04Tests.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2023;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NebulousIndustries.AdventOfCode.Year2023;
+
+[TestClass]
+public class Day04Tests : DayTests<Day04>
+{
+    public override long Part1Answer => 21158;
+
+    public override long Part2Answer => 6050769;
+}
---
+++ src/AdventOfCode/Year2023/Day04.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="Day04.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Year2023;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Day04 : DayBase<ScratchCard>
+{
+    public override long Part1()
+    {
+        return this.GetInput()
+            .Sum(s => s.PointValue);
+    }
+
+    public override long Part2()
+    {
+        List<ScratchCard> cards = this.GetInput().ToList();
+        List<int> cardAmounts = new(Enumerable.Repeat(1, cards.Count));
+        for (int i = 0; i < cards.Count; i++)
+        {
+            for (int j = 0; j < cards[i].Matches; j++)
+            {
+                cardAmounts[i + j + 1] += cardAmounts[i];
+            }
+        }
+        return cardAmounts.Sum();
+    }
+}
+
+public class ScratchCard : IDayInput
+{
+    public List<int> WinningNumbers { get; } = [];
+
+    public List<int> Numbers { get; } = [];
+
+    public int Matches { get => this.Numbers.Intersect(this.WinningNumbers).Count(); }
+
+    public int PointValue
+    {
+        get
+        {
+            return this.Matches > 0 ? (int)Math.Pow(2, this.Matches - 1) : 0;
+        }
+    }
+
+    public bool Load(string input)
+    {
+        string[] inputs = input.Split([":", "|"], StringSplitOptions.TrimEntries);
+        this.WinningNumbers.AddRange(inputs[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+        this.Numbers.AddRange(inputs[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+        return false;
+    }
+}
