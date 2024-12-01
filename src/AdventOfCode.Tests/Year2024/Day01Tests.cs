b3ac0494c6ad90ed2cad585713d05784d4845172
Jeff Weiner <nebula@planet-nebula.com>
2024-12-01 00:16:19 -0500

    2024 Day 1 complete

---
+++ src/AdventOfCode.Tests/Year2024/Day01Tests.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Day01Tests.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2024;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NebulousIndustries.AdventOfCode.Year2024;
+
+[TestClass]
+public class Day01Tests : DayTests<Day01>
+{
+    public override long Part1Answer => 2742123;
+
+    public override long Part2Answer => 21328497;
+}
---
+++ src/AdventOfCode/Year2024/Day01.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="Day01.cs" company="Nebulous Industries">
+// Copyright (c) Nebulous Industries. All rights reserved.
+// </copyright>
+
+namespace NebulousIndustries.AdventOfCode.Year2024;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Day01 : DayBase<NumberPair>
+{
+    public override long Part1()
+    {
+        IEnumerable<int> left = this.GetInput().Select(p => p.Left).OrderBy(n => n);
+        IEnumerable<int> right = this.GetInput().Select(p => p.Right).OrderBy(n => n);
+        IEnumerable<int> distances = left.Zip(right).Select(p => Math.Abs(p.First - p.Second));
+        return distances.Sum();
+    }
+
+    public override long Part2()
+    {
+        IEnumerable<int> left = this.GetInput().Select(p => p.Left);
+        IEnumerable<int> right = this.GetInput().Select(p => p.Right);
+        return left.Select(l => l * right.Count(r => r == l)).Sum();
+    }
+}
+
+public class NumberPair : IDayInput
+{
+    public int Left { get; set; }
+
+    public int Right { get; set; }
+
+    public bool Load(string input)
+    {
+        string[] numbers = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        this.Left = int.Parse(numbers[0]);
+        this.Right = int.Parse(numbers[1]);
+        return false;
+    }
+}
