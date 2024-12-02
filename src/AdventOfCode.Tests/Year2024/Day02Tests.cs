f96c817445953afff25f5663711d05fa707207e8
Jeff Weiner <nebula@planet-nebula.com>
2024-12-02 15:10:16 -0500

    2024 Day 2 complete

---
+++ src/AdventOfCode.Tests/Year2024/Day02Tests.cs
@@ -0,0 +1,11 @@
+﻿using NebulousIndustries.AdventOfCode.Year2024;
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2024;
+
+[TestClass]
+public class Day02Tests : DayTests<Day02>
+{
+    public override long Part1Answer => 326;
+
+    public override long Part2Answer => 381;
+}
---
+++ src/AdventOfCode/Year2024/Day02.cs
@@ -0,0 +1,64 @@
+﻿namespace NebulousIndustries.AdventOfCode.Year2024;
+
+public class Day02 : DayBase<ReactorLevels>
+{
+    public override long Part1()
+    {
+        return this.GetInput().Count(r => r.AreLevelsSafe(0));
+    }
+
+    public override long Part2()
+    {
+        return this.GetInput().Count(r => r.AreLevelsSafe(1));
+    }
+}
+
+public class ReactorLevels : IDayInput
+{
+    public List<int> Levels { get; } = [];
+
+    public bool AreLevelsSafe(int allowableProblems)
+    {
+        int increasingCount = 0;
+        for (int i = 1; i < this.Levels.Count; i++)
+        {
+            if (this.Levels[i - 1] < this.Levels[i])
+            {
+                increasingCount++;
+            }
+        }
+
+        bool isIncreasing = increasingCount > 1;
+        for (int i = 1; i < this.Levels.Count; i++)
+        {
+            bool stillIncreasing = isIncreasing && this.Levels[i - 1] <= this.Levels[i];
+            bool stillDecreasing = !isIncreasing && this.Levels[i - 1] >= this.Levels[i];
+            bool withinRange = Math.Abs(this.Levels[i - 1] - this.Levels[i]) is (> 0 and < 4);
+            if (!(stillIncreasing || stillDecreasing) || !withinRange)
+            {
+                bool safeWithLeftRemoved = false;
+                bool safeWithRightRemoved = false;
+                if (allowableProblems > 0)
+                {
+                    ReactorLevels leftRemoved = new();
+                    leftRemoved.Levels.AddRange(this.Levels.Where((l, idx) => idx != i - 1));
+                    safeWithLeftRemoved = leftRemoved.AreLevelsSafe(allowableProblems - 1);
+
+                    ReactorLevels rightRemoved = new();
+                    rightRemoved.Levels.AddRange(this.Levels.Where((l, idx) => idx != i));
+                    safeWithRightRemoved = rightRemoved.AreLevelsSafe(allowableProblems - 1);
+                }
+
+                return safeWithLeftRemoved || safeWithRightRemoved;
+            }
+        }
+
+        return true;
+    }
+
+    public bool Load(string input)
+    {
+        this.Levels.AddRange(input.Split(' ').Select(int.Parse));
+        return false;
+    }
+}
