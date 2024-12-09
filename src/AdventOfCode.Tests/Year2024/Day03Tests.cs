3452f91f82979097289a5d148a2f1db6714cf90a
Jeff Weiner <nebula@planet-nebula.com>
2024-12-08 18:11:17 -0800

    2024 Day 3 complete

---
+++ src/AdventOfCode.Tests/Year2024/Day03Tests.cs
@@ -0,0 +1,11 @@
+﻿using NebulousIndustries.AdventOfCode.Year2024;
+
+namespace NebulousIndustries.AdventOfCode.Tests.Year2024;
+
+[TestClass]
+public class Day03Tests : DayTests<Day03>
+{
+    public override long Part1Answer => 159833790;
+
+    public override long Part2Answer => 89349241;
+}
---
+++ src/AdventOfCode/Year2024/Day03.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NebulousIndustries.AdventOfCode.Year2024;
+
+public class Day03 : DayBase<Multiplications>
+{
+    public override long Part1()
+    {
+        return this.GetInput().Single().Tuples.Sum(t => t.Item1 * t.Item2);
+    }
+
+    public override long Part2()
+    {
+        Multiplications.ProcessDosAndDonts = true;
+        return this.GetInput().Single().Tuples.Sum(t => t.Item1 * t.Item2);
+    }
+}
+
+public class Multiplications : IDayInput
+{
+    public static bool ProcessDosAndDonts { get; set; }
+
+    public List<Tuple<int, int>> Tuples { get; } = [];
+
+    private bool Dont { get; set; }
+
+    public Regex MulPattern { get; } = new(@"(mul\(([0-9]+),([0-9]+)\))|(do\(\))|(don't\(\))");
+
+    public bool Load(string input)
+    {
+        for (Match match = MulPattern.Match(input); match.Success; match = MulPattern.Match(input, match.Index + 1))
+        {
+            if (match.Groups[5].Success)
+            {
+                Dont = true;
+            }
+            else if (match.Groups[4].Success)
+            {
+                Dont = false;
+            }
+            else if (!Dont || !ProcessDosAndDonts)
+            {
+                Tuples.Add(Tuple.Create(int.Parse(match.Groups[2].Value), int.Parse(match.Groups[3].Value)));
+            }
+        }
+
+        return true;
+    }
+}
